781c46d88a54126de2d3d28acba3b4928b4fd8b6
wenbo <wenbo@DESKTOP-5FI74H8>
2020-04-19 15:07:55 +0800

    InsideCppObjectModel 项目工程结构

---
+++ trunk/src/InsideCppObjectModel/InsideCppObjectModel/Chapter03/VirtualInheritance.cpp
@@ -0,0 +1,14 @@
+#include "VirtualInheritance.h"
+#include <iostream>
+
+using namespace Chapter03;
+
+VirtualInheritance::VirtualInheritance()
+{
+	std::cout << "VirtualInheritance Constructor!" << std::endl;
+}
+
+VirtualInheritance::~VirtualInheritance()
+{
+	std::cout << "VirtualInheritance DeConstructor!" << std::endl;
+}
---
+++ trunk/src/InsideCppObjectModel/InsideCppObjectModel/Chapter03/VirtualInheritance.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Chapter03
+{
+	class VirtualInheritance
+	{
+	public:
+		VirtualInheritance();
+		~VirtualInheritance();
+	};
+}
+
